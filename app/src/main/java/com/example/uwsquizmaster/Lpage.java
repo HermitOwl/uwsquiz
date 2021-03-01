a16bea9063c0ae723612c51a72b014713a165dc8
steba <stebar0205@hotmail.co.uk>
2021-03-01 00:00:39 +0000

    Pre Alpha app *Login page designed *Login Page tested for data entry *Register page Linked to Login Via textView

---
+++ app/src/main/java/com/example/uwsquizmaster/Lpage.java
@@ -0,0 +1,14 @@
+package com.example.uwsquizmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lpage);
+    }
+}
